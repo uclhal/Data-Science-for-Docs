00de7ce0eac5e994fde0bfff8127e7dbe66da134
aal-hind <a.al-hindawi@imperial.ac.uk>
2016-12-05 11:54:41 +0000

    updated session analysis to give 3 graphs:
    1) histogram of number of people who gave feedback per session
    2) Scores per session
    3) scores by attendee (slightly cheeky!)

--- feedback_analysis/analysis.R
+++ feedback_analysis/analysis.R
@@ -1,4 +1,12 @@
 library(googlesheets)
 library(dplyr)
+library(ggplot2)
 
-ddata <- gs_url("https://docs.google.com/spreadsheets/d/1phoNhmy4aVb7QHYV9uHH7l9hKRRZcAdOY8eK1sqPzFo/edit?usp=sharing") %>% gs_read_csv()
+ddata <- gs_url("https://docs.google.com/spreadsheets/d/1phoNhmy4aVb7QHYV9uHH7l9hKRRZcAdOY8eK1sqPzFo/edit?usp=sharing") %>% gs_read()
+names(ddata) <- c("time", "session", "nickname", "score")
+
+ddata %>% filter(!is.na(score)) %>% group_by(session) %>% ggplot(aes(x=session)) + geom_histogram(stat="count")
+
+ddata %>% filter(!is.na(score)) %>% group_by(session) %>% ggplot(aes(x=session, y=score)) + geom_boxplot() + ylim(0,10)
+
+ddata %>% filter(!is.na(score)) %>% group_by(nickname) %>% ggplot(aes(x=nickname, y=score)) + geom_boxplot() + ylim(0, 10)
